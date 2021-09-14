b8fcbbbc9460338786ec7e35d1ac8d20f8cac906
JmPaunlagui <johnpaunlagui@gmail.com>
2021-09-14 14:44:27 +0800

    Added the pages using react-router-dom

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 //Navbar
 import Navbar from './components/Navbar'
 
+// Pages
+import Contact from "./pages/Contact";
+import Home from './pages/Home';
+import PageNotFound from './pages/PageNotFound';
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+import Services from "./pages/Services";
+
 function App() {
-  return (
-    <div>
-      <Navbar/>
-    </div> 
-  );
+    return (
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path = '/' exact render={() => <Home/>}/>
+          <Route path = '/contact' exact render={() => <Contact/>}/>
+          <Route path = '/projects' exact render={() => <Projects/>} />
+          <Route path = '/resume' exact render={() => <Resume/>}/>
+          <Route path = '/services' exact render={() => <Services/>}/>
+          <Route component={PageNotFound} />
+        </Switch>
+      </Router>
+    );
 }
 
 export default App;

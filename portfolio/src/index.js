d278b057da9998f0f99687a863c484e99cfa2ab5
JmPaunlagui <johnpaunlagui@gmail.com>
2021-10-02 19:29:11 +0800

    Path renamed

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import './assets/main.css'
+import './Assets/Main.css'
 
 ReactDOM.render(
   <React.StrictMode>
